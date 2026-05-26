830ba6c8cab980055246b7cc01dc913afb35773c
mlkenny <michaelk1734@gmail.com>
2026-05-26 14:18:27 -0700

    Initial commit

---
+++ packages/shared-types/index.ts
@@ -0,0 +1,13 @@
+// Simple User Type (for importing to backend/frontend, will change later).
+export type User = {
+    id: string;
+    email: string;
+    name: string
+};
+
+// Simple Post Type.
+export type Post = {
+    id: string;
+    title: string;
+    content: string
+};
